e04f287a65e52bd8cf30d25dcbf61f58ff711f8a
Andre M <amore@kth.se>
2025-05-28 11:06:36 +0200

    Initial

--- custom_components/nimly_digital_lock/__init__.py
+++ custom_components/nimly_digital_lock/__init__.py
@@ -1,1 +1,12 @@
 """Nimly Digital Lock integration."""
+
+from .const import DOMAIN
+
+async def async_setup(hass, config):
+    """Set up the integration via YAML (if supported)."""
+    return True
+
+async def async_setup_entry(hass, entry):
+    """Set up integration from config flow entry."""
+    # Set up platforms here if you have any (lock, sensor)
+    return True
